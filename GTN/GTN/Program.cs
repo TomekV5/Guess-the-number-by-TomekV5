1e9cd6b14a6ae8e731d27b7517fea5af704e9257
TomekV5 <138574138+TomekV5@users.noreply.github.com>
2024-05-04 10:46:54 +0300

    GTN

---
+++ GTN/GTN/Program.cs
@@ -0,0 +1,41 @@
+﻿﻿using System;
+
+namespace GTN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomnumber = new Random();
+            int computernumber = randomnumber.Next(1, 101);
+
+            while (true)
+            {
+                Console.Write("Guess a number (1-100): ");
+                string playerinput = Console.ReadLine();
+                bool isValid = int.TryParse(playerinput, out int playerNumber);
+                if (isValid)
+                {
+                    if (playerNumber == computernumber)
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                    else if (playerNumber > computernumber)
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                    break;
+                }
+            }
+        }
+    }
+}
